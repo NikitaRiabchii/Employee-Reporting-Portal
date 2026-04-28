1bd1e92b8e1ad7ec199a68d0c5475d0cd47b6cf2
Nikita <nikitariabchii@gmail.com>
2026-04-28 15:14:02 +0200

    reorganize structure according to common convetions of C programs, add sqlite, precompile some test

--- web-server-core-tests/web-core-tests.c
+++
@@ -1,2 +0,0 @@
-int main(){return 0;}
-//here will be some uni-tests of modules and all core logic witch emulation of user acctivity
---
+++ web-server-core/tests/web-core-tests.c
@@ -0,0 +1,1 @@
+//here will be some uni-tests of modules and all core logic witch emulation of user acctivity
